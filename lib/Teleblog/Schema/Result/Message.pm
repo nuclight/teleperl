f62a3e882ec4f765a9a27a0d6fd0d5bfb30a51c0
Vasiliy Terkin <v.gadfly@gmail.com>
2019-02-02 17:18:42 +0100

    added more fields to messages and uid as pri key

--- lib/Teleblog/Schema/Result/Message.pm
+++ lib/Teleblog/Schema/Result/Message.pm
@@ -23,6 +23,12 @@ __PACKAGE__->table("messages");
 
 =head1 ACCESSORS
 
+=head2 uid
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
 =head2 id
 
   data_type: 'integer'
@@ -54,9 +60,36 @@ __PACKAGE__->table("messages");
   datetime_undef_if_invalid: 1
   is_nullable: 1
 
+=head2 action
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 media
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 reply_to
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=head2 fwd_from
+
+  data_type: 'bigint'
+  is_nullable: 1
+
+=head2 via_bot_id
+
+  data_type: 'bigint'
+  is_nullable: 1
+
 =cut
 
 __PACKAGE__->add_columns(
+  "uid",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
   "id",
   { data_type => "integer", is_nullable => 1 },
   "from_id",
@@ -73,12 +106,52 @@ __PACKAGE__->add_columns(
     datetime_undef_if_invalid => 1,
     is_nullable => 1,
   },
+  "action",
+  { data_type => "text", is_nullable => 1 },
+  "media",
+  { data_type => "text", is_nullable => 1 },
+  "reply_to",
+  { data_type => "integer", is_nullable => 1 },
+  "fwd_from",
+  { data_type => "bigint", is_nullable => 1 },
+  "via_bot_id",
+  { data_type => "bigint", is_nullable => 1 },
 );
 
+=head1 PRIMARY KEY
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2019-01-31 14:34:47
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:4tloH6V4nwX9cyoc9yzAGg
+=over 4
+
+=item * L</uid>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("uid");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<msg_id>
+
+=over 4
+
+=item * L</id>
+
+=item * L</from_id>
+
+=item * L</to_id>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("msg_id", ["id", "from_id", "to_id"]);
 
 
-__PACKAGE__->add_unique_constraint(msg_id => [ qw/id from_id to_id/ ]);
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2019-02-02 16:43:56
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:nSQqyQDMInjnT0t6izqGmw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
